30c2fa87757232fa9e5cec1483f9584ab4354bb1
selviaji <ajithngc@gmail.com>
2017-04-19 14:14:52 +0530

    Add files via upload

---
+++ aji.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ int a;
+ printf("enter the value")
+ scanf("%d",&a)
+if(a==0)
+ printf("zero");
+ if(a>0)
+ printf("postive");
+ else
+ printf("negative");
+ }
+
